d7aefd9529c7376f1e704e33454af09631e500f8
shreepornoCoder <sontos1979@gmail.com>
2024-11-08 00:56:00 +0600

    file added

---
+++ Class-1/problem3.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+
+elif n % 3 == 0:
+    print("Fizz")
+
+elif n % 5 == 0:
+    print("Buzz")
+
+else:
+    print("Not a FizzBuzz number")
