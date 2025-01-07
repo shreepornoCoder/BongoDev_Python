e9589431d54f4a403ca8af818ce1f84bded26694
shreepornoCoder <sontos1979@gmail.com>
2025-01-07 23:42:45 +0600

    Django Project Added

--- Django_Project/AMARSHOP/amarshop/amarshop/settings.py
+++ Django_Project/AMARSHOP/amarshop/amarshop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b$2k&tpan1bgyhywt0%*9xpe4mzq9!7^ua=aznvau&+p_%wh#)'
+SECRET_KEY = 'django-insecure-*i$7@6mfo$5j&mkeyohiwf^jdr$yxiy(2d0y8b)0j5(d117@76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'user',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +75,8 @@ WSGI_APPLICATION = 'amarshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
