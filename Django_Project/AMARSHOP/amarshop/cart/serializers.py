386b749f86d3409f69fc4fa2fa7210fd06f5b97e
shreepornoCoder <sontos1979@gmail.com>
2025-01-29 23:30:50 +0600

    Changes Added

--- Django_Project/AMARSHOP/amarshop/cart/serializers.py
+++ Django_Project/AMARSHOP/amarshop/cart/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 from .models import Cart, CartItems, Saved_Items
 from products.serializers import ProductListSerializer
+from products.models import Product
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    product_id = serializers.IntegerField(write_only=True)
+    price = serializers.DecimalField(decimal_places=2, read_only=True, source='get_price')
+
     class Meta:
         model = CartItems
         fields = ['id', 'product', 'product_id', 'quantity', 'price']
         read_only_fields = ['id', 'price']
+
+    def validate_quantity(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Quantity must be set atleast 1!") 
+        raise value
+    
+    def validate_stock(self, data):
+        product_id = self.data.get('product_id')
+        quantity = self.data.get('quantity')
+
+        try:
+            product = Product.objects.get(id=product_id)
+            if quantity < product.stock:
+                raise serializers.ValidationError(f"Only {product.stock} is available in the stock!")
+            raise data
+        except Product.DoesNotExist:
+            raise serializers.ValidationError(f"Product doesn't found!")
