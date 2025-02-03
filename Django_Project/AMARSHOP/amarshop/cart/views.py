fd53fb8808ac6c6262f3a780b10ba0116439a4a9
shreepornoCoder <sontos1979@gmail.com>
2025-02-03 22:32:09 +0600

    Changes Added

--- Django_Project/AMARSHOP/amarshop/cart/views.py
+++ Django_Project/AMARSHOP/amarshop/cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from . models import Cart, CartItems, Saved_Items
+from . serializers import CartSerializer, CartItemSerializer, SavedItemSerializer
 
-# Create your views here.
+class CartItemViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+    
+    def get_or_create_cart(self):
+        cart, created = Cart.objects.get_or_create(user=self.request.user)
